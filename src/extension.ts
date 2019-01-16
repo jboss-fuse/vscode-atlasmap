ac1e728d43c73dddc53d9cc862eb831bc6148898
Lars Heinemann <lhein.smx@gmail.com>
2019-01-16 12:14:30 +0100

    Open AtlasMap command asks for url and port #19
    Signed-off-by: Lars Heinemann <lhein.smx@gmail.com>

--- src/extension.ts
+++ src/extension.ts
@@ -10,8 +10,51 @@ export function activate(context: vscode.ExtensionContext) {
 	var path = require('path');
 	var atlasmapExecutablePath = context.asAbsolutePath(path.join('jars','atlasmap-standalone.jar'));
 
-	context.subscriptions.push(vscode.commands.registerCommand('atlasmap.open', () => {
-		opn('http://127.0.0.1:8585');
+	context.subscriptions.push(vscode.commands.registerCommand('atlasmap.open', async () => {
+
+		// obtain the stored configuration settings for url and port
+		const urlFromSettings = vscode.workspace.getConfiguration().get("atlasmap.url");
+		const portFromSettings = vscode.workspace.getConfiguration().get("atlasmap.port");
+
+		// obtain the atlasmap url
+		var url = await vscode.window.showInputBox(
+			{ 
+				prompt: 'Enter the url of your AtlasMap instance (without port).',
+				value: '' + urlFromSettings
+			}
+		);
+
+		// obtain the atlasmap port
+		var port = await vscode.window.showInputBox(
+			{ 
+				prompt: 'Provide glob pattern of files to have empty last line.',
+				value: '' + portFromSettings
+			}
+		);
+
+		var setUrlAsGlobal = vscode.workspace.getConfiguration().inspect("atlasmap.url").workspaceValue == undefined;
+		var setPortAsGlobal = vscode.workspace.getConfiguration().inspect("atlasmap.port").workspaceValue == undefined;
+
+		if (url !== urlFromSettings) {
+			// url changed - update the configuration to remember the url
+			vscode.workspace.getConfiguration().update("atlasmap.url", url, setUrlAsGlobal);
+		}
+
+		if (port !== portFromSettings) {
+			// port changed - update the configuration to remember the port
+			vscode.workspace.getConfiguration().update("atlasmap.port", port, setPortAsGlobal);
+		}
+
+		if (!url.startsWith("http://") && !url.startsWith("https://")) {
+			url = "http://" + url;
+		}
+
+		if (url.lastIndexOf(":") > 4) {
+			// url contains a port ? cut it off
+			url = url.substring(0, url.lastIndexOf(":"));
+		}
+
+		opn(url + ':' + port);
 	}));
 
 	context.subscriptions.push(vscode.commands.registerCommand('atlasmap.start', () => {
