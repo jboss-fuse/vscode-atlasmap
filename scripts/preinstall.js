709c5aaf9e3a1ffbb3c3feae7c784822b88a7a21
Aurélien Pupier <apupier@redhat.com>
2019-01-16 09:23:42 +0100

    retrieve AtlasMap jar at build time #16
    - it avoids to commit the jar in source code
    - the drawback is that it requires to call "npm install --ignore-script"
    before calling a classic npm install
    
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

---
+++ scripts/preinstall.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var atlasmap_server_version = "1.39.0";
+
+const download = require("mvn-artifact-download").default;
+const fs = require('fs');
+const path = require('path');
+
+download('io.atlasmap:atlasmap-standalone:' + atlasmap_server_version, './jars/').then((filename)=>{
+	fs.renameSync(filename, path.join('.', 'jars', 'atlasmap-standalone.jar'));
+});
