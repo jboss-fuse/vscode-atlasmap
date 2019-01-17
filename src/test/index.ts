6dea3b19e7dea917c22e84bde31a90c4af2d57f9
Lars Heinemann <lhein.smx@gmail.com>
2019-01-17 14:27:33 +0100

    first draft to fix and extend the tests (not working yet) #19
    Signed-off-by: Lars Heinemann <lhein.smx@gmail.com>

--- src/test/index.ts
+++ src/test/index.ts
@@ -5,7 +5,8 @@ import * as testRunner from 'vscode/lib/testrunner';
 testRunner.configure({
 	ui: 'bdd',
 	useColors: true,
-	timeout: 100000
+	timeout: 100000,
+	slow: 50000
 });
 
 module.exports = testRunner;
