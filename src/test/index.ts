fddbd61e042e66f14a7d8ae1274ab6e9fb9752f3
Lars Heinemann <lhein.smx@gmail.com>
2019-01-21 15:48:29 +0100

    removed obsolete exception handler #19
    Signed-off-by: Lars Heinemann <lhein.smx@gmail.com>

--- src/test/index.ts
+++ src/test/index.ts
@@ -2,10 +2,6 @@
 
 import * as testRunner from "vscode/lib/testrunner";
 
-process.on("unhandledRejection", err => {
-	console.log("Unhandled rejection:", err);
-});
-
 testRunner.configure({
 	ui: "bdd",
 	useColors: true,
