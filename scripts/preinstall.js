ab4432949ac1eae891e0bfccdec683f6e6247e43
Lars Heinemann <lhein.smx@gmail.com>
2019-03-05 11:48:45 +0100

    downloading adm file from release tags #7
    Signed-off-by: Lars Heinemann <lhein.smx@gmail.com>

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -1,10 +1,10 @@
 'use strict';
 
-var atlasmap_server_version = "1.39.4";
-
 const download = require("mvn-artifact-download").default;
 const fs = require('fs');
 const path = require('path');
+
+const atlasmap_server_version = process.env.npm_package_config_atlasmapversion;
 
 download('io.atlasmap:atlasmap-standalone:' + atlasmap_server_version, './jars/').then((filename)=>{
 	fs.renameSync(filename, path.join('.', 'jars', 'atlasmap-standalone.jar'));
--- src/test/command.start.test.ts
+++ src/test/command.start.test.ts
@@ -3,17 +3,16 @@
 import * as atlasMapWebView from "../atlasMapWebView";
 import * as chai from "chai";
 import * as child_process from 'child_process';
+import * as fs from "fs";
 import * as sinon from "sinon";
 import * as sinonChai from "sinon-chai";
 import * as testUtils from "./command.test.utils";
 import * as vscode from "vscode";
 import { BrowserType } from "../utils";
+import { RESTART_CHOICE, WARN_MSG } from '../extension';
 
-const fs = require("fs");
 const expect = chai.expect;
 chai.use(sinonChai);
-
-const WARN_MSG:string = "There is currently a local AtlasMap instance running. We need to restart that instance. Make sure you have saved all your changes in the AtlasMap UI to prevent data loss.";
 
 testUtils.BROWSER_TYPES.forEach(function (browserConfig) {
 	describe("Start AtlasMap Command Tests with browser type: " + browserConfig, function() {
@@ -34,7 +33,7 @@ testUtils.BROWSER_TYPES.forEach(function (browserConfig) {
 			showWarningMessageStub = sinon.stub(vscode.window, "showWarningMessage");
 			showWarningMessageStub.withArgs(WARN_MSG, sinon.match.any).callsFake((args) => {
 				console.log("Restart warning message shown: " + args);
-			}).returns("Restart");
+			}).returns(RESTART_CHOICE);
 			showWarningMessageStub.callThrough();
 			showInformationMessageSpy = sinon.spy(vscode.window, "showInformationMessage");
 			createOutputChannelSpy = sinon.spy(vscode.window, "createOutputChannel");
