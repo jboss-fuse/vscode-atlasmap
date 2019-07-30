0e4172eaf79c2f3830ffb1ea3ea2b7887e99e6d6
jkopriva <jkopriva@redhat.com>
2019-07-30 11:53:13 +0200

    First UI tests for AtlasMap (Fixes #140)
    Signed-off-by: Josef Kopriva <jkopriva@redhat.com>

---
+++ src/ui-test/common/conditions.ts
@@ -0,0 +1,38 @@
+import { Workbench, Notification, NotificationType } from 'vscode-extension-tester';
+import { views } from './constants';
+
+export async function getNotificationWithMessage(message: string) {
+	try {
+		const center = await new Workbench().openNotificationsCenter();
+		const notifications = await center.getNotifications(NotificationType.Any);
+		for (const item of notifications) {
+			const text = await item.getMessage();
+			if (text.indexOf(message) > -1) {
+				return item;
+			}
+		}
+		return null;
+	} catch (err) {
+		//do not print err
+		return null;
+	}
+}
+
+export async function whilegetNotificationWithMessage(message: string) {
+	return !(await getNotificationWithMessage(message));
+}
+
+export async function atlasMapWindowExists(): Promise<boolean | undefined> {
+	try {
+		const titles = await new Workbench().getEditorView().getOpenEditorTitles();
+		for (const title of titles) {
+			if (title.indexOf(views.ATLASMAP_TITLE) > -1) {
+				return true;
+			}
+		}
+		return false;
+	} catch (err) {
+		//do not print err
+		return false;
+	}
+}
---
+++ src/ui-test/common/utils.ts
@@ -0,0 +1,26 @@
+import { Workbench, EditorView } from 'vscode-extension-tester';
+import { commands, views } from './constants';
+
+export async function startAtlasMap() {
+	await new Workbench().executeCommand(commands.START_ATLASMAP);
+	await new Promise((res) => { setTimeout(res, 1000); });
+}
+
+export async function stopAtlasMap() {
+	await new Workbench().executeCommand(commands.STOP_ATLASMAP);
+	await new Promise((res) => { setTimeout(res, 1000); });
+}
+
+export async function atlasMapTabIsAccessible(){
+	await new EditorView().openEditor(views.ATLASMAP_TITLE);
+}
+
+export async function clearNotifications(){
+	try {
+		const center = await new Workbench().openNotificationsCenter();
+		await center.clearAllNotifications();
+	} catch (err) {
+		console.log(err);
+		return null;
+	}
+}
