e7e192a49872d7ceb805e7e4acf518396815ef8a
Josef Kopriva <jkopriva@redhat.com>
2019-09-10 09:56:34 +0200

    Build failures fix for ui-tests (Fixes #171)
    Signed-off-by: Josef Kopriva <jkopriva@redhat.com>

--- src/ui-test/basic-test.ts
+++ src/ui-test/basic-test.ts
@@ -8,7 +8,7 @@ import { startAtlasMap, stopAtlasMap, atlasMapTabIsAccessible, clearNotification
 export function basicTests() {
 	let driver: WebDriver;
 
-	describe('Start AtlasMap and verify correct notifications', () => {
+	describe('Start/Stop AtlasMap and verify correct notifications', () => {
 		beforeEach(async () => {
 			driver = VSBrowser.instance.driver;
 			await clearNotifications();
@@ -17,6 +17,7 @@ export function basicTests() {
 		it('Start Command should show a notification with the correct text', async function () {
 			this.timeout(30000);
 			await startAtlasMap();
+			driver.sleep(1000);
 			const notificationStarting = await driver.wait(() => { return getNotificationWithMessage(notifications.ATLASMAP_STARTING); }, 20000) as Notification;
 			expect(await notificationStarting.getMessage()).contains(notifications.ATLASMAP_STARTING);
 			await driver.wait(() => { return whilegetNotificationWithMessage(notifications.ATLASMAP_WAITING); }, 20000);
@@ -24,36 +25,28 @@ export function basicTests() {
 			await new EditorView().closeEditor(views.ATLASMAP_TITLE);
 		});
 
-		describe('Start AtlasMap (when running) and verify correct notifications', () => {
-			it('Second Start Command should open AtlasMap window', async function () {
-				this.timeout(20000);
-				await startAtlasMap();
-				await driver.wait(() => { return atlasMapWindowExists(); }, 10000);
-				await atlasMapTabIsAccessible();
-			});
-
+		it('Second Start Command should open AtlasMap window', async function () {
+			this.timeout(20000);
+			await startAtlasMap();
+			await driver.wait(() => { return atlasMapWindowExists(); }, 10000);
+			await atlasMapTabIsAccessible();
 		});
 
-		describe('Stop AtlasMap and verify correct notification', () => {
-			it('Stop Command should show a notification with the correct text', async function () {
-				this.timeout(20000);
-				await stopAtlasMap();
-				const notification = await driver.wait(() => { return getNotificationWithMessage(notifications.ATLASMAP_STOPPED); }, 20000);
-				expect(await notification.getMessage()).contains(notifications.ATLASMAP_STOPPED);
-			});
-
+		it('Stop Command should show a notification with the correct text', async function () {
+			this.timeout(30000);
+			driver.sleep(1000);
+			await stopAtlasMap();
+			driver.sleep(1000);
+			const notification = await driver.wait(() => { return getNotificationWithMessage(notifications.ATLASMAP_STOPPED); }, 20000);
+			expect(await notification.getMessage()).contains(notifications.ATLASMAP_STOPPED);
 		});
 
-		describe('Stop already stopped AtlasMap and verify correct notification', () => {
-			it('Second Stop Command should show a notification with the correct text', async function () {
-				this.timeout(20000);
-				await stopAtlasMap();
-				const notification = await driver.wait(() => { return getNotificationWithMessage(notifications.ATLASMAP_UNABLE_LOCATE); }, 10000) as Notification;
-				expect(await notification.getMessage()).contains(notifications.ATLASMAP_UNABLE_LOCATE);
-			});
-
+		it('Second Stop Command should show a notification with the correct text', async function () {
+			this.timeout(30000);
+			await stopAtlasMap();
+			const notification = await driver.wait(() => { return getNotificationWithMessage(notifications.ATLASMAP_UNABLE_LOCATE); }, 10000) as Notification;
+			expect(await notification.getMessage()).contains(notifications.ATLASMAP_UNABLE_LOCATE);
 		});
-
 
 		afterEach(async () => {
 			await clearNotifications();
