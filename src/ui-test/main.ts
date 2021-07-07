6f3278c9498b886be8289d6d324d718af20d202c
Marian Lorinc <mlorinc@redhat.com>
2021-07-07 14:40:21 +0200

    Try path workaround
    Signed-off-by: Marian Lorinc <mlorinc@redhat.com>

--- src/ui-test/main.ts
+++ src/ui-test/main.ts
@@ -1,40 +1,23 @@
 import { ExTester, ReleaseQuality } from "vscode-extension-tester";
 import * as path from "path";
 
-const storageFolder = undefined;
+const storageFolder = path.resolve('..', 'test-resources');
 const releaseQuality = ReleaseQuality.Stable;
 const extensionsDir = path.resolve('.', 'test-extensions'); 
 const codeVersion = undefined;
 const testsGlob = 'out/ui-test/all.js';
-const interval = 2000;
 
 let devToolsActivePortAttemptsCount = 5;
 
-async function runTests(tester: ExTester, resolve: Function, reject: Function): Promise<void> {
-	try {
-		process.exitCode = await tester.runTests(testsGlob);
-		resolve(process.exitCode);
-	} catch (error) {
-		if (error.name === 'WebDriverError' && error.message.includes('unknown error: DevToolsActivePort file')) {
-			if (devToolsActivePortAttemptsCount > 0) {
-				setTimeout(runTests, interval, tester, resolve, reject);
-				devToolsActivePortAttemptsCount--;
-			}
-			else {
-				reject(error);
-			}
-		}
-	}
+async function runTests(tester: ExTester): Promise<void> {
+	process.exitCode = await tester.runTests(testsGlob);
 }
 
 async function main() {
 	const tester = new ExTester(storageFolder, releaseQuality, extensionsDir);
 	await tester.downloadCode(codeVersion);
 	await tester.downloadChromeDriver(codeVersion);
-	
-	return await new Promise(async (resolve, reject) => {
-		setImmediate(runTests, tester, resolve, reject);
-	});
+	return await runTests(tester);
 }
 
 main();
