d8200dfba1da28fad2e59079db6d5de55cc2a2d5
JoshiRaez <jraez@redhat.com>
2022-09-14 18:21:06 +0200

    Created test for the new use case

--- src/test/commands/atlasmap.file.create.test.ts
+++ src/test/commands/atlasmap.file.create.test.ts
@@ -19,8 +19,9 @@ describe.only('Test Command: atlasmap.file.create', function() {
 	let workspaceSelectorStub: sinon.SinonStub;
 	let admLocationStub: sinon.SinonStub;
 	let fileNameInputStub: sinon.SinonStub;
-	
-	const testADMFileName: string = 'test.adm';
+	let dirPickerWindow: sinon.SinonStub;
+
+	let testADMFileName: string = 'test.adm';
 	let testADMFile: vscode.Uri;
 	let workspaceFolderUri: vscode.Uri;
 	let workspaceFolder: vscode.WorkspaceFolder;
@@ -30,18 +31,20 @@ describe.only('Test Command: atlasmap.file.create', function() {
 
 		workspaceFolderUri = vscode.Uri.file(path.join(__dirname, '../../../test Fixture with speci@l chars'));
 		workspaceFolder = vscode.workspace.getWorkspaceFolder(workspaceFolderUri);
-		
+
 		workspaceSelectorStub = sinon.stub(vscode.window, 'showWorkspaceFolderPick');
-		workspaceSelectorStub.returns(workspaceFolder);
 		admLocationStub = sinon.stub(vscode.window, 'showQuickPick');
-		admLocationStub.returns("");
 		fileNameInputStub = sinon.stub(vscode.window, 'showInputBox');
+		dirPickerWindow = sinon.stub(vscode.window, 'showOpenDialog');
+
 	});
 
 	after( async () => {
 		sandbox.restore();
 		workspaceSelectorStub.restore();
 		fileNameInputStub.restore();
+		admLocationStub.restore();
+		dirPickerWindow.restore();
 
 		if (testADMFile && await fileExists(testADMFile)) {
 			await vscode.workspace.fs.delete(testADMFile);
@@ -51,10 +54,15 @@ describe.only('Test Command: atlasmap.file.create', function() {
 	afterEach( async () => {
 		workspaceSelectorStub.resetHistory();
 		fileNameInputStub.resetHistory();
+		admLocationStub.resetHistory();
 	});
 
 	it('Test execution of command and creation of file', async function() {
+				
+		workspaceSelectorStub.returns(workspaceFolder);
+		admLocationStub.returns("");
 		fileNameInputStub.onFirstCall().returns(testADMFileName);
+
 		try {
 			await vscode.commands.executeCommand('atlasmap.file.create');
 			testADMFile = vscode.Uri.file(`${workspaceFolderUri.path}/${testADMFileName}`);
@@ -67,34 +75,30 @@ describe.only('Test Command: atlasmap.file.create', function() {
 		}
 	});
 	
-	it.only('Test execution of command and creation of file', async function() {
+	it('Test execution of command and creation of file on different directory', async function() {
 
 		const tempFolder = "admTmp";
 		const tempFolderUri = path.join(workspaceFolder.uri.path, tempFolder);
 		fs.mkdirSync(tempFolderUri);
 
-		workspaceSelectorStub = sinon.stub(vscode.window, 'showWorkspaceFolderPick');
 		workspaceSelectorStub.returns(workspaceFolder);
-		admLocationStub = sinon.stub(vscode.window, 'showQuickPick');
 		admLocationStub.returns("Select a folder inside workspace");
-		const dirPickerWindow = sinon.stub(vscode.window, 'showOpenDialog');
-		dirPickerWindow.resolves([vscode.Uri.file(".")] as vscode.Uri[]);
-		fileNameInputStub = sinon.stub(vscode.window, 'showInputBox');
+		dirPickerWindow.resolves([vscode.Uri.file(tempFolderUri)] as vscode.Uri[]);
 		fileNameInputStub.onFirstCall().returns(testADMFileName);
 
 		try {
 			await vscode.commands.executeCommand('atlasmap.file.create');
-			testADMFile = vscode.Uri.file(`${workspaceFolderUri.path}/${testADMFileName}`);
+			testADMFile = vscode.Uri.file(`${tempFolderUri}/${testADMFileName}`);
 			expect(workspaceSelectorStub.called, 'Workspace Selector has not been called').to.be.true;
 			expect(admLocationStub.called, 'Adm location selection has not been called').to.be.true;
 			expect(dirPickerWindow.called, 'Window directory picker was not called').to.be.true;
 			expect(fileNameInputStub.called, 'File name has not been asked').to.be.true;
-			expect(await fileExists(vscode.Uri.file(path.join(tempFolderUri, testADMFileName))),
-			 'The created file does not exist').to.be.true;
+			expect(await fileExists(testADMFile),
+				'The created file does not exist').to.be.true;
 		} catch (err) {
 			fail(err);
 		} finally {
-			fs.rmdirSync(tempFolderUri, {force: true, recursive: true})
+			fs.rmSync(tempFolderUri, {force:true, recursive: true});
 		}
 	});
 
